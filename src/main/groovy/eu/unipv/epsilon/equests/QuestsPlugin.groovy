dd2298dc523d897bdb99f1d68ad199e224d43dd5
Luca Zanussi <luca.z@outlook.com>
2015-05-28 16:26:55 +0200

    Added classes (templates) support

--- src/main/groovy/eu/unipv/epsilon/equests/QuestsPlugin.groovy
+++ src/main/groovy/eu/unipv/epsilon/equests/QuestsPlugin.groovy
@@ -2,7 +2,10 @@ package eu.unipv.epsilon.equests
 
 import org.gradle.api.*
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.StopExecutionException
 import org.gradle.api.tasks.bundling.Zip
 import org.yaml.snakeyaml.Yaml
 
@@ -12,18 +15,28 @@ class QuestsPlugin implements Plugin<Project> {
     public static final String QUEST_DESCRIPTION_FILENAME = 'quest-description'
     public static final String PACK_EXT = 'eqc'
 
-    public static final String TEMP_PATH = 'tmp/eqcfiles'
+    public static final String EQC_SOURCE_PATH = 'src/main/eqc'
+
+    public static final String TEMP_PATH = 'eqcfiles'
     public static final String OUT_PATH = 'eqcs'
 
     void apply(Project project) {
-        project.getPluginManager().apply(BasePlugin.class)
+        // BasePlugin has the clean task, but is already extended by JavaPlugin
+        //project.getPluginManager().apply(BasePlugin.class)
+
+        // Use JavaPlugin to compile templates, if any (generate 1.6 or 1.7 classes to be compatible with dex)
+        project.getPluginManager().apply(JavaPlugin.class)
+        project.sourceCompatibility = '1.7'
+        project.targetCompatibility = '1.7'
 
         // Add extension objects
         project.extensions.create('questCollection', CollectionMetaExt)
 
         // Add tasks
+
+        // Generates metadata
         Task genMetadata = project.task('genMetadata') {
-            inputs.dir 'src/eqc'
+            inputs.dir EQC_SOURCE_PATH
             inputs.file 'build.gradle'
             outputs.file "$project.buildDir/$TEMP_PATH/metadata.yaml"
 
@@ -37,8 +50,9 @@ class QuestsPlugin implements Plugin<Project> {
             }
         }
 
-        Task processSource = project.task(type: Copy, 'processSource') {
-            from 'src/eqc'
+        // Copies and renames data to output, excluding quest-description files
+        Task processQuests = project.task(type: Copy, 'processQuests') {
+            from EQC_SOURCE_PATH
             into "$project.buildDir/$TEMP_PATH"
             exclude "*/$QUEST_DESCRIPTION_FILENAME"
         } << { Copy task ->
@@ -51,26 +65,55 @@ class QuestsPlugin implements Plugin<Project> {
                     it.renameTo("$task.destinationDir/quests/$dirName")
                 }
                 catch (Exception e) {
-                    if (it.name != 'quests') println("Unrecognized dir: $it.name")
+                    if (it.name != 'quests' && it.name != 'classes')
+                        println("Unrecognized dir: $it.name")
                 }
             }
         }
 
-        Task pack = project.task(type: Zip, dependsOn: [processSource, genMetadata], 'pack') {
+        // Moves classes to output dir for packaging
+        Task moveClasses = project.task(type: Copy, dependsOn: 'classes', 'moveClasses') {
+            from project.sourceSets.main.output.classesDir,
+                 project.sourceSets.main.output.resourcesDir
+            into "$project.buildDir/$TEMP_PATH/classes"
+        }
+
+        // Compiles dex classes if dx is on the classpath
+        Task dexClasses = project.task(dependsOn: moveClasses, 'dexClasses') {
+            // Not using Exec task so we can catch failures
+
+            // NOT WORKING!?
+            //   inputs.dir   "$project.buildDir/$TEMP_PATH/classes"
+            //   outputs.file "$project.buildDir/$TEMP_PATH/classes.dex"
+
+            doLast {
+                def proc = Runtime.runtime.exec(
+                        'cmd /c dx --dex --output="classes.dex" "classes"', null, new File("$project.buildDir/$TEMP_PATH"))
+                proc.waitFor()
+
+                if (proc.exitValue() != 0) {
+                    println "warning: \"dx\" could not be found in the classpath, " +
+                            "any template in this collection will not work on Android"
+                    throw new StopExecutionException();
+                }
+            }
+        }
+
+        Task pack = project.task(type: Zip, dependsOn: [processQuests, moveClasses, dexClasses, genMetadata], 'pack') {
             baseName project.name
             extension PACK_EXT
             destinationDir project.file("$project.buildDir/$OUT_PATH")
-            from processSource.destinationDir
+            from processQuests.destinationDir
             // exclude '**/*.txt'
         }
 
-        [genMetadata, processSource, pack].each { it.group = GROUP_NAME }
+        [genMetadata, processQuests, pack].each { it.group = GROUP_NAME }
 
         project.build.dependsOn pack
     }
 
     static Map<String, Object> searchQuestsMeta(Project p) {
-        def root = p.file('src/eqc')
+        def root = p.file(EQC_SOURCE_PATH)
 
         def descFiles = p.fileTree(root).include("*/$QUEST_DESCRIPTION_FILENAME")
         def questDirs = descFiles.collect { it.parentFile }
